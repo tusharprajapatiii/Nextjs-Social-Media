87d308c123b5a18613f696465d48bee813d380d7
tushar <prajapatitushar789@gmail.com>
2022-04-05 19:15:19 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com", "i.pravatar.cc"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://nextjs-social-media-theta.vercel.app/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
