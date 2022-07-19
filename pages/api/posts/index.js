52ec3476c3fa9f1261176c3ae979b3e80a5c300c
tushar <prajapatitushar789@gmail.com>
2022-07-20 00:23:20 +0530

    fix

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -8,6 +8,7 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
+  secure: true,
 });
 export default async function handler(req, res) {
   await dbconnect();
